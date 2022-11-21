765616fc08fcbeeca4043f38f7933d14bfaf9667
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-11-21 09:58:46 +0100

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by David Koch on 21.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
